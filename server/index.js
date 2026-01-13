d8349277d2bafe27ae4f26b9ec38b78448fae4e2
Abhaya <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2026-01-13 13:39:06 +0530

    wcw

--- server/index.js
+++ server/index.js
@@ -35,8 +35,14 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(cors())
-
+app.use(
+    cors({
+        origin: [process.env.CLIENT_URL],
+        credentials: true,
+        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+        allowedHeaders: ["Content-Type", "Authorization"],
+    })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
