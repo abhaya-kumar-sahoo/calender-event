974dd0751308367f1d891f1c171a89a58cf74820
Abhaya <61579704+abhaya-kumar-sahoo@users.noreply.github.com>
2026-01-13 13:46:00 +0530

    weew

--- server/index.js
+++ server/index.js
@@ -35,14 +35,20 @@ app.use((req, res, next) => {
     next();
 });
 
-app.use(
-    cors({
-        origin: [process.env.CLIENT_URL],
-        credentials: true,
-        methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-        allowedHeaders: ["Content-Type", "Authorization"],
-    })
-);
+const corsOptions = {
+    origin: (origin, callback) => {
+        if (!origin) return callback(null, true); // server-to-server
+        if (origin === process.env.CLIENT_URL) {
+            return callback(null, true);
+        }
+        return callback(new Error("Not allowed by CORS"));
+    },
+    credentials: true,
+};
+
+app.use(cors(corsOptions));
+app.options("*", cors(corsOptions));
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
